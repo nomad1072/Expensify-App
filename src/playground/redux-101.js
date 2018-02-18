0caa008e3a6c9f3128ce874a42483927f41882f0
nomad1072 <siddharthlanka@gmail.com>
2018-02-18 23:32:12 +0530

    Added dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -5,32 +5,51 @@ const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       console.log('INCREMENT');
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
       console.log('DECREMENT');
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case 'RESET':
       console.log('RESET');
       return {
         count: 0
       };
+    case 'SET':
+      return {
+        count: action.count
+      }
     default:
       return state;
   }
 });
+// subscribe is called when any change is made to store
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
+
 // Actions - increment, decrement, reset
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
+unsubscribe();
 store.dispatch({
   type: 'RESET'
 });
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'DECREMENT',
+  decrementBy: 10
 });
+
+store.dispatch({
+  type: 'SET',
+  count: 15
+})
 
 console.log(store.getState());
