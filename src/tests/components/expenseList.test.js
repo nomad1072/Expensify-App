39385ea466ce95dba6fb81540b99bff499aa0a45
nomad1072 <siddharthlanka@gmail.com>
2018-02-23 18:06:23 +0530

    Removed jest configs and enzyme

--- src/tests/components/expenseList.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { ExpenseList } from '../../components/ExpenseList';
-
-test('should render ExpenseList with expenses', () => {
-  const wrapper = shallow();
-});
